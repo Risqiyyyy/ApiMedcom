4b7be72695b3aabfe7ebcda13f4a9c86fc7db308
Risqiyyyy <raisafebriani05@gmail.com>
2023-10-04 02:29:06 +0700

    update

--- index.js
+++ index.js
@@ -47,10 +47,6 @@ app.use(JurusanRoute);
 app.use(MapelRoute);
 // store.sync();
 
-app.get('/', (req, res) => {
-    res.send('Hey this is my API running 🥳')
-  })
-
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server up and running...');
 });
